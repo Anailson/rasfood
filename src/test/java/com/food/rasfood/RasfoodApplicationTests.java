5d5b435e826d6d1a4b94801e168bbf87249bc0e9
anailson <arsantos315@gmail.com>
2025-04-19 10:43:24 -0300

    projeto jpa food

---
+++ src/test/java/com/food/rasfood/RasfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.rasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
